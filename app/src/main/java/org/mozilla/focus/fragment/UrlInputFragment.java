33eab106cf1f754d33fe62155f5436514fa7de3d
Sebastian Kaspari <s.kaspari@gmail.com>
2017-07-26 13:30:26 +0200

    Work in progress: Restructure UI to make searching / typing a URL faster. (#832)

--- app/src/main/java/org/mozilla/focus/fragment/UrlInputFragment.java
+++ app/src/main/java/org/mozilla/focus/fragment/UrlInputFragment.java
@@ -8,14 +8,19 @@ package org.mozilla.focus.fragment;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.PopupMenu;
 import android.text.SpannableString;
 import android.text.style.StyleSpan;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
@@ -24,7 +29,9 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import org.mozilla.focus.R;
+import org.mozilla.focus.activity.InfoActivity;
 import org.mozilla.focus.autocomplete.UrlAutoCompleteFilter;
+import org.mozilla.focus.locale.LocaleAwareAppCompatActivity;
 import org.mozilla.focus.telemetry.TelemetryWrapper;
 import org.mozilla.focus.utils.ThreadUtils;
 import org.mozilla.focus.utils.UrlUtils;
@@ -35,7 +42,7 @@ import org.mozilla.focus.widget.InlineAutocompleteEditText;
 /**
  * Fragment for displaying he URL input controls.
  */
-public class UrlInputFragment extends Fragment implements View.OnClickListener, InlineAutocompleteEditText.OnCommitListener, InlineAutocompleteEditText.OnFilterListener {
+public class UrlInputFragment extends Fragment implements View.OnClickListener, InlineAutocompleteEditText.OnCommitListener, InlineAutocompleteEditText.OnFilterListener, PopupMenu.OnMenuItemClickListener {
     public static final String FRAGMENT_TAG = "url_input";
 
     private static final String ARGUMENT_URL = "url";
@@ -44,28 +51,17 @@ public class UrlInputFragment extends Fragment implements View.OnClickListener, 
     private static final String ARGUMENT_Y = "y";
     private static final String ARGUMENT_WIDTH = "width";
     private static final String ARGUMENT_HEIGHT = "height";
+    private static final String ARGUMENT_TRANSLUCENT = "translucent";
 
-    private static final String ANIMATION_HOME_SCREEN = "home_screen";
     private static final String ANIMATION_BROWSER_SCREEN = "browser_screen";
 
     private static final int ANIMATION_DURATION = 200;
 
-    /**
-     * Create a new UrlInputFragment and animate the url input view from the position/size of the
-     * fake url bar view.
-     */
-    public static UrlInputFragment createWithHomeScreenAnimation(View fakeUrlBarView) {
-        int[] screenLocation = new int[2];
-        fakeUrlBarView.getLocationOnScreen(screenLocation);
+    public static UrlInputFragment createWithBackground() {
+        final Bundle arguments = new Bundle();
+        arguments.putBoolean(ARGUMENT_TRANSLUCENT, false);
 
-        Bundle arguments = new Bundle();
-        arguments.putString(ARGUMENT_ANIMATION, ANIMATION_HOME_SCREEN);
-        arguments.putInt(ARGUMENT_X, screenLocation[0]);
-        arguments.putInt(ARGUMENT_Y, screenLocation[1]);
-        arguments.putInt(ARGUMENT_WIDTH, fakeUrlBarView.getWidth());
-        arguments.putInt(ARGUMENT_HEIGHT, fakeUrlBarView.getHeight());
-
-        UrlInputFragment fragment = new UrlInputFragment();
+        final UrlInputFragment fragment = new UrlInputFragment();
         fragment.setArguments(arguments);
 
         return fragment;
@@ -79,6 +75,7 @@ public class UrlInputFragment extends Fragment implements View.OnClickListener, 
         final Bundle arguments = new Bundle();
         arguments.putString(ARGUMENT_ANIMATION, ANIMATION_BROWSER_SCREEN);
         arguments.putString(ARGUMENT_URL, url);
+        arguments.putBoolean(ARGUMENT_TRANSLUCENT, true);
 
         int[] screenLocation = new int[2];
         urlView.getLocationOnScreen(screenLocation);
@@ -104,6 +101,9 @@ public class UrlInputFragment extends Fragment implements View.OnClickListener, 
     private HintFrameLayout urlInputContainerView;
     private View urlInputBackgroundView;
     private View toolbarBackgroundView;
+    private View menuView;
+
+    private @Nullable PopupMenu displayedPopupMenu;
 
     private volatile boolean isAnimating;
 
@@ -130,15 +130,20 @@ public class UrlInputFragment extends Fragment implements View.OnClickListener, 
         urlView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
+                Log.w("SKDBG", "onFocusChange(): " + hasFocus + " isAnimating: " + isAnimating);
+
                 // Avoid showing keyboard again when returning to the previous page by back key.
                 if (hasFocus && !isAnimating) {
                     ViewUtils.showKeyboard(urlView);
+                    Log.e("SKDBG", "SHOW KEYBOARD!!");
                 }
             }
         });
 
         toolbarBackgroundView = view.findViewById(R.id.toolbar_background);
         urlInputBackgroundView = view.findViewById(R.id.url_input_background);
+
+        menuView = view.findViewById(R.id.menu);
 
         urlInputContainerView = (HintFrameLayout) view.findViewById(R.id.url_input_container);
         urlInputContainerView.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
@@ -152,6 +157,16 @@ public class UrlInputFragment extends Fragment implements View.OnClickListener, 
             }
         });
 
+        if (isOverlay()) {
+            view.findViewById(R.id.brand_background).setVisibility(View.GONE);
+        } else {
+            dismissView.setVisibility(View.GONE);
+            toolbarBackgroundView.setVisibility(View.GONE);
+
+            menuView.setVisibility(View.VISIBLE);
+            menuView.setOnClickListener(this);
+        }
+
         urlView.setOnCommitListener(this);
 
         if (getArguments().containsKey(ARGUMENT_URL)) {
@@ -162,9 +177,17 @@ public class UrlInputFragment extends Fragment implements View.OnClickListener, 
         return view;
     }
 
+    private boolean isOverlay() {
+        return getArguments().getBoolean(ARGUMENT_TRANSLUCENT, false);
+    }
+
     public boolean onBackPressed() {
-        animateAndDismiss();
-        return true;
+        if (isOverlay()) {
+            animateAndDismiss();
+            return true;
+        }
+
+        return false;
     }
 
     @Override
@@ -190,17 +213,36 @@ public class UrlInputFragment extends Fragment implements View.OnClickListener, 
                 animateAndDismiss();
                 break;
 
+            case R.id.menu:
+                final PopupMenu popupMenu = new PopupMenu(view.getContext(), view);
+                popupMenu.getMenuInflater().inflate(R.menu.menu_home, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(this);
+                popupMenu.setGravity(Gravity.TOP);
+                popupMenu.show();
+                displayedPopupMenu = popupMenu;
+                break;
+
             default:
                 throw new IllegalStateException("Unhandled view in onClick()");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        // On detach, the PopupMenu is no longer relevant to other content (e.g. BrowserFragment) so dismiss it.
+        // Note: if we don't dismiss the PopupMenu, its onMenuItemClick method references the old Fragment, which now
+        // has a null Context and will cause crashes.
+        if (displayedPopupMenu != null) {
+            displayedPopupMenu.dismiss();
         }
     }
 
     private void animateFirstDraw() {
         final String animation = getArguments().getString(ARGUMENT_ANIMATION);
 
-        if (ANIMATION_HOME_SCREEN.equals(animation)) {
-            playHomeScreenAnimation(false);
-        } else if (ANIMATION_BROWSER_SCREEN.equals(animation)) {
+        if (ANIMATION_BROWSER_SCREEN.equals(animation)) {
             playBrowserScreenAnimation(false);
         }
     }
@@ -221,107 +263,11 @@ public class UrlInputFragment extends Fragment implements View.OnClickListener, 
 
         final String animation = getArguments().getString(ARGUMENT_ANIMATION);
 
-        if (ANIMATION_HOME_SCREEN.equals(animation)) {
-            playHomeScreenAnimation(true);
-        } else if (ANIMATION_BROWSER_SCREEN.equals(animation)) {
+        if (ANIMATION_BROWSER_SCREEN.equals(animation)) {
             playBrowserScreenAnimation(true);
         } else {
             dismiss();
         }
-    }
-
-    /**
-     * Play animation between home screen and the URL input.
-     */
-    private void playHomeScreenAnimation(final boolean reverse) {
-        if (isAnimating) {
-            // We are already animating, let's ignore another request.
-            return;
-        }
-
-        isAnimating = true;
-
-        int[] screenLocation = new int[2];
-        urlInputContainerView.getLocationOnScreen(screenLocation);
-
-        int leftDelta = getArguments().getInt(ARGUMENT_X) - screenLocation[0];
-        int topDelta = getArguments().getInt(ARGUMENT_Y) - screenLocation[1];
-
-        float widthScale = (float) getArguments().getInt(ARGUMENT_WIDTH) / urlInputContainerView.getWidth();
-        float heightScale = (float) getArguments().getInt(ARGUMENT_HEIGHT) / urlInputContainerView.getHeight();
-
-        if (!reverse) {
-            // Move all views to the position of the fake URL bar on the home screen. Hide them until
-            // the animation starts because we need to switch between fake URL bar and the actual URL
-            // bar once the animation starts.
-            urlInputContainerView.setAlpha(0);
-            urlInputContainerView.setPivotX(0);
-            urlInputContainerView.setPivotY(0);
-            urlInputContainerView.setScaleX(widthScale);
-            urlInputContainerView.setScaleY(heightScale);
-            urlInputContainerView.setTranslationX(leftDelta);
-            urlInputContainerView.setTranslationY(topDelta);
-            urlInputContainerView.setAnimationOffset(1.0f);
-
-            toolbarBackgroundView.setAlpha(0);
-
-            dismissView.setAlpha(0);
-        }
-
-        // Move the URL bar from its position on the home screen to the actual position (and scale it).
-        urlInputContainerView.animate()
-                .setDuration(ANIMATION_DURATION)
-                .scaleX(reverse ? widthScale : 1)
-                .scaleY(reverse ? heightScale : 1)
-                .translationX(reverse ? leftDelta : 0)
-                .translationY(reverse ? topDelta : 0)
-                .setInterpolator(new DecelerateInterpolator())
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationStart(Animator animation) {
-                        ViewUtils.updateAlphaIfViewExists(getActivity(), R.id.fake_urlbar, 0f);
-
-                        urlInputContainerView.setAlpha(1);
-
-                        if (reverse) {
-                            urlView.setText("");
-                            urlView.setCursorVisible(false);
-                            urlView.clearFocus();
-                        }
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        if (reverse) {
-                            urlInputContainerView.setAlpha(0f);
-
-                            ViewUtils.updateAlphaIfViewExists(getActivity(), R.id.fake_urlbar, 1f);
-
-                            dismiss();
-                        } else {
-                            urlView.setCursorVisible(true);
-                        }
-
-                        isAnimating = false;
-                    }
-                });
-
-        final ObjectAnimator hintAnimator = ObjectAnimator.ofFloat(
-                urlInputContainerView, "animationOffset", reverse ? 0f : 1f, reverse ? 1f : 0f);
-
-        hintAnimator.setDuration(ANIMATION_DURATION);
-        hintAnimator.start();
-
-        // Let the toolbar background come int from the top
-        toolbarBackgroundView.animate()
-                .alpha(reverse ? 0 : 1)
-                .setDuration(ANIMATION_DURATION)
-                .setInterpolator(new DecelerateInterpolator());
-
-        // Use an alpha animation on the transparent black background
-        dismissView.animate()
-                .alpha(reverse ? 0 : 1)
-                .setDuration(ANIMATION_DURATION);
     }
 
     private void playBrowserScreenAnimation(final boolean reverse) {
@@ -490,8 +436,13 @@ public class UrlInputFragment extends Fragment implements View.OnClickListener, 
         if (searchText.trim().isEmpty()) {
             clearView.setVisibility(View.GONE);
             searchViewContainer.setVisibility(View.GONE);
+
+            if (!isOverlay()) {
+                menuView.setVisibility(View.VISIBLE);
+            }
         } else {
             clearView.setVisibility(View.VISIBLE);
+            menuView.setVisibility(View.GONE);
 
             final String hint = getString(R.string.search_hint, searchText);
 
@@ -500,6 +451,35 @@ public class UrlInputFragment extends Fragment implements View.OnClickListener, 
 
             searchView.setText(content);
             searchViewContainer.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        final int id = item.getItemId();
+
+        switch (id) {
+            case R.id.settings:
+                ((LocaleAwareAppCompatActivity) getActivity()).openPreferences();
+                return true;
+
+            case R.id.about:
+                Intent aboutIntent = InfoActivity.getAboutIntent(getActivity());
+                startActivity(aboutIntent);
+                return true;
+
+            case R.id.rights:
+                Intent rightsIntent = InfoActivity.getRightsIntent(getActivity());
+                startActivity(rightsIntent);
+                return true;
+
+            case R.id.help:
+                Intent helpIntent = InfoActivity.getHelpIntent(getActivity());
+                startActivity(helpIntent);
+                return true;
+
+            default:
+                throw new IllegalStateException("Unhandled view ID in onMenuItemClick()");
         }
     }
 }
